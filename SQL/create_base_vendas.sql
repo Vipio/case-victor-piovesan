e301933401f1f2f5fbb8d9dd44e2cf581c644c44
Vipio <vipio@live.com>
2024-04-19 01:05:22 -0300

    att create_base_vendas
    adicionando proc para limpar dados duplicados

--- SQL/create_base_vendas.sql
+++ SQL/create_base_vendas.sql
@@ -6,3 +6,18 @@ LINHA	    STRING,
 DATA_VENDA	TIMESTAMP,		
 QTD_VENDA	INTEGER,
 );
+
+
+CREATE OR REPLACE PROCEDURE boticario.proc_bases_vendas()
+  OPTIONS(strict_mode=false)
+  BEGIN
+    CREATE OR REPLACE TABLE `case-boti-420516.boticario.bases_vendas`
+    AS
+      SELECT ID_MARCA, MARCA, ID_LINHA, LINHA, DATA_VENDA, QTD_VENDA
+      FROM (
+        SELECT ID_MARCA, MARCA, ID_LINHA, LINHA, DATA_VENDA, QTD_VENDA,
+              ROW_NUMBER() OVER (PARTITION BY ID_MARCA, MARCA, ID_LINHA, LINHA, DATA_VENDA, QTD_VENDA ORDER BY ID_MARCA, MARCA, ID_LINHA, LINHA, DATA_VENDA, QTD_VENDA) AS row_number
+        FROM `case-boti-420516.boticario.bases_vendas`
+    )
+    WHERE row_number = 1;
+  END;
